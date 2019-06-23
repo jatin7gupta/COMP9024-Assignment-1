ffc304a7fe4d07e377860f4f8512f5fd556f4818
Jatin Gupta <jatin.gupta@student.unsw.edu.au>
2019-06-23 14:01:03 +1000

    dropped binary search

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+  
+
 void print_board(int *array, int length) {
 	for (int j = 0; j < length; j++) {
 		if (*(array + j) == -1) {
@@ -10,6 +12,7 @@ void print_board(int *array, int length) {
 		}
 	}
 }
+
 
 int input(int *board) {
 	
@@ -34,9 +37,9 @@ int input(int *board) {
 	                      sum = 10 * sum + (c - '0'); 
 	                      c = getchar();
 		              }
-		              board[board_length++] = sum;
+		              *(board + board_length++) = sum;
 		              break;
-		    case 'b': board[board_length++] = -1;
+		    case 'b': *(board + board_length++) = -1;
 		    case ' ':
 		    case '\t':
 		    case '\n':
@@ -48,21 +51,43 @@ int input(int *board) {
 	return board_length;
 }
 
+int search(int *board, int length) {
+//TODO
+	int return_value = 0;
+	return return_value;
+}
+
+
+int check_board_validity(int *board, int length) {
+	int result = search(board,length);
+	//TODO
+	return result;
+}
+
 int main(void) {
 	
 	
 	int start_board[4];
 	int length_start_board = input(start_board);
 	
+	int start_board_validity = check_board_validity(start_board, length_start_board);
+	printf("state %d", start_board_validity);
+	/*
 	int goal_board[4];
 	int length_goal_board = input(goal_board);
 	
 	if (length_start_board < 0 || length_goal_board < 0) {
 		return EXIT_FAILURE;
 	}
-	printf("\nstart:");
-	print_board(start_board, length_start_board);
+	
+	int goal_board_validity = check_board_validity(goal_board, length_goal_board);
+	
+	
 	printf("\ngoal:");
 	print_board(goal_board, length_goal_board);
+	*/
+	printf("\nstart:");
+	print_board(start_board, length_start_board);
+	
 	return EXIT_SUCCESS;
 }
